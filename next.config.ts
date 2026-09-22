5ab3d704468dac78e06ad52a79d64ca3ae0b69e3
khalfaoui chiheb eddine <barcaca1999@gmail.com>
2026-09-22 20:30:10 +0100

    Prepare Docker deployment to Azure Web App

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,13 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = { images: { remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }] } };
+const nextConfig: NextConfig = {
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+    ],
+  },
+};
+
 export default nextConfig;
